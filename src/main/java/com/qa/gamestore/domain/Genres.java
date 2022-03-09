dd5f17e4995b5ec13be42505fc91421c9ec9549f
Kalliszta <kallisztag@gmail.com>
2022-03-09 15:56:35 +0000

    GS-65  #in-progress #comment Created basic Genre class

---
+++ src/main/java/com/qa/gamestore/domain/Genres.java
@@ -0,0 +1,43 @@
+package com.qa.gamestore.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+
+	@Entity
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Data
+	public class Genres {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@NonNull
+		private String genre;
+		
+		@JsonManagedReference
+		@OneToMany(mappedBy= "genres", fetch = FetchType.LAZY)
+		@OnDelete(action = OnDeleteAction.CASCADE) //if deleted so are its children
+		private List<GameGenres> gameGenres;
+		
+		public void updateFields(Platforms newInfo) {
+			this.genre = newInfo.getName();
+		}
+}
