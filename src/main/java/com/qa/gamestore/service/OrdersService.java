203e2f6246faf3d45c371e7f25e94dc5a7634b68
Kalliszta <kallisztag@gmail.com>
2022-03-09 12:05:06 +0000

    GS-49 #done #comment Created basic structure for sevice

--- src/main/java/com/qa/gamestore/service/OrdersService.java
+++ src/main/java/com/qa/gamestore/service/OrdersService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qa.gamestore.domain.Orders;
-import com.qa.gamestore.repo.AccountsRepo;
 import com.qa.gamestore.repo.OrdersRepo;
 
 @Service
