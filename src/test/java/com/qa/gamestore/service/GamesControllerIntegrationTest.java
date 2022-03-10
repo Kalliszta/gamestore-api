de9b1184a47e8d296d12184c41ca80aded91ea2e
Kalliszta <kallisztag@gmail.com>
2022-03-10 17:42:48 +0000

    GS-63 #in-progress #comment Integration testing for /create request successful

--- src/test/java/com/qa/gamestore/service/GamesControllerIntegrationTest.java
+++ src/test/java/com/qa/gamestore/service/GamesControllerIntegrationTest.java
@@ -6,10 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.gamestore.domain.Games;
@@ -32,8 +38,21 @@ import com.qa.gamestore.domain.Games;
 	
 	@Test
 	void testCreate() throws Exception {
-		Games testGame = new Games(0L, "TestGame", "A game that doesn't exist", 7, 5.87D, 0);
+		Games testGame = new Games(0L, "TestGame", "A game that doesn't exist", 7, 5.87, false);
+		Games expectedGame = new Games(5L, "TestGame", "A game that doesn't exist", 7, 5.87, false);
 		
+		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+				.request(HttpMethod.POST, URL + "/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonifier.writeValueAsString(testGame))
+				.accept(MediaType.APPLICATION_JSON);
+		
+		ResultMatcher status = MockMvcResultMatchers.status().isCreated();
+		ResultMatcher content = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedGame));
+		
+		this.mock.perform(mockRequest).andExpect(status).andExpect(content);
+	
 	}
 	
 }
