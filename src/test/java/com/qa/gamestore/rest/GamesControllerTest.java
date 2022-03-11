6be59a924e3fc005e1a867bf8c2c5bb985420446
Kalliszta <kallisztag@gmail.com>
2022-03-11 12:06:00 +0000

    GS-76 #in-progress #comment The get() method in GamesController which returns all games has been tested successfully using unit testing

--- src/test/java/com/qa/gamestore/rest/GamesControllerTest.java
+++ src/test/java/com/qa/gamestore/rest/GamesControllerTest.java
@@ -2,6 +2,8 @@ package com.qa.gamestore.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -48,6 +50,29 @@ public class GamesControllerTest {
 		
 		//verify
 		Mockito.verify(this.service, Mockito.times(1)).create(newGame);
+	}
+	
+	@Test
+	void testReadAll() {
+		//given
+		//set up using setUpForEach
+		List<Games> expectedGames = Arrays.asList(
+				savedGame,
+				new Games(2L, "Elder Scrolls", "An RPG", 18, 15.99, true),
+				new Games(3L, "Horizon Zero Dawn", "An RPG that takes place in the future", 16, 29.99, false),
+				new Games(4L, "Horizon Forbidden West", "An RPG that takes place in the future", 16, 79.99, false),
+				new Games(5L, "Minecraft", "A fun game to play with friends", 7, 19.99, true),
+				new Games(6L, "Animal Crossing New Horizons", "The most relaxing game ever", 3, 45.25, true),
+				new Games(7L, "Elder Scrolls", "Skyrim", 18, 32.65, false)
+				);
+		//when
+		Mockito.when(this.service.readAll()).thenReturn(expectedGames);
+		ResponseEntity<List<Games>> response = new ResponseEntity<List<Games>>(expectedGames, HttpStatus.OK);
+		//then
+		assertThat(response).isEqualTo(this.controller.get());
+		
+		//verify
+		Mockito.verify(this.service, Mockito.times(1)).readAll();
 	}
 	
 	@Test
