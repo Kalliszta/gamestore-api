f658ac39a540e38756176f35f040159d44b9ed50
Kalliszta <kallisztag@gmail.com>
2022-03-14 13:42:01 +0000

    GS-17 #done #comment Users can now succesfully add multiples items to one order

--- src/main/java/com/qa/gamestore/domain/Orders.java
+++ src/main/java/com/qa/gamestore/domain/Orders.java
@@ -73,7 +73,7 @@ public class Orders {
 		if (accounts != null) {
 			return this.accounts.getId();
 		} else {
-		return accountsId;
+			return accountsId;
 		}
 	}
 
--- src/main/java/com/qa/gamestore/rest/OrdersController.java
+++ src/main/java/com/qa/gamestore/rest/OrdersController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.qa.gamestore.domain.Games;
 import com.qa.gamestore.domain.OrderGames;
 import com.qa.gamestore.domain.Orders;
 import com.qa.gamestore.service.OrdersService;
